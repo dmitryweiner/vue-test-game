033e4114d9f2a3a8344494a1a9eb5b045962830c
AlishaNyam <64323382+AlishaNyam@users.noreply.github.com>
2020-05-16 20:03:55 +0700

    tests

--- src/components/AnswerForm.spec.js
+++ src/components/AnswerForm.spec.js
@@ -5,14 +5,11 @@ describe("AnswerForm.vue", () => {
   test("renders question", () => {
     const answer = "Test answer";
     const handler = jest.fn();
-    const wrapper = shallowMount(AnswerForm, {
-      propsData: { handler }
-    });
+    const wrapper = shallowMount(AnswerForm);
     wrapper.find('input[type="text"]').setValue(answer);
     expect(handler).not.toBeCalled();
     wrapper.find('button').trigger('click');
-    expect(handler).toBeCalled();
-    expect(handler).toHaveBeenCalledTimes(1);
-    expect(handler).toHaveBeenCalledWith([answer]); // TODO?
+    expect(wrapper.emitted('checked')).toBeTruthy()
+    expect(wrapper.emitted('checked').length).toBe(1)
   });
 });
